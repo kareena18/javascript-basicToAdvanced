8aa32618ba29eabccaaaac85ff86a6850d24a6e2
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-23 17:18:00 +0000

    datatypes

---
+++ DataTypes/03_datatypes.js
@@ -0,0 +1,25 @@
+//data types of value
+
+//7 data types
+//number, string, boolean, null, undefined, object, symbol
+
+let marks = 12;
+let personName = "kareena";
+let isLogged = false;
+let score = null;
+let points;
+
+console.log(typeof marks);
+console.log(typeof personName);
+console.log(typeof isLogged);
+console.log(typeof score);
+console.log(typeof points);
+
+let arr = [1, 2, 3, 4, 5];
+console.log(typeof arr);
+
+let obj = {
+    name: "kareena",
+    age: 21
+}
+console.log(typeof obj);
