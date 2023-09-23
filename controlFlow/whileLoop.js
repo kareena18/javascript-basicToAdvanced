e060481f952d63ce83733ab47356acc5ea4173e5
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-23 18:43:10 +0000

    while loop

---
+++ controlFlow/whileLoop.js
@@ -0,0 +1,13 @@
+//while loop
+let i=1;
+while(i<=5){
+    console.log("while loop",i);
+    i++;
+}
+
+let dish=["biryani","chai","coffee","dosa","upma"];
+let j=0;
+while(j<dish.length){
+    console.log(dish[j]);
+    j++;
+}
