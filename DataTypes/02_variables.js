0366a06171fe2d65c5a3636a3ab5cac8ce8823fe
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-23 17:04:17 +0000

    variables

---
+++ DataTypes/02_variables.js
@@ -0,0 +1,11 @@
+//variables, let and constants
+
+var score = 34;
+var score = 56;//redeclare
+console.log(score);
+
+let points = 23;//reassign possible
+console.log(points);
+
+const value = 10;
+console.log(value);
