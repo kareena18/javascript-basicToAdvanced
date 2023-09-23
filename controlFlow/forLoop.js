979a15e6432f70fc351d2db8303fbdcb3e797cbc
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-23 18:38:51 +0000

    for loop

---
+++ controlFlow/forLoop.js
@@ -0,0 +1,16 @@
+//for loop
+
+for(let i=0;i<10;i++){
+    console.log("run the task",i);
+}
+
+//usecase of for loop
+let dish=["biryani","chai","coffee","dosa","upma"];
+
+//access the array value
+for(let i=0;i<dish.length;i++){
+    console.log(dish[i]);
+}
+
+//iteration and value of i
+//0,1,2,3,4.............
