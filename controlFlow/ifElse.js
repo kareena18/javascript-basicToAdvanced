5f4fc9083dab544250bdd964bfc2c7c00756657c
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-23 18:49:57 +0000

    if else

---
+++ controlFlow/ifElse.js
@@ -0,0 +1,15 @@
+// if elseif else 
+let budget = 3450;
+if (budget > 6000) {
+    console.log("will do dinner in hotel");
+}
+else if (budget > 4000) {
+    console.log("will do dinner in 3 star hotel");
+}
+else if (budget > 3000) {
+    console.log("will do dinner in 2 star hotel");
+}
+else {
+    console.log("we can't eat")
+}
+console.log("i am outside the conditional block");
