4841e7e2abadc9b543e89a29bf642a2ebd2f7113
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-23 18:53:04 +0000

    nested if

---
+++ controlFlow/nestedIf.js
@@ -0,0 +1,11 @@
+//nested if else
+let num =23;
+if(num>22){
+    console.log("num is greater than 22");
+    if(num>25){
+        console.log("num is greater than 25");
+    }
+    else{
+        console.log("num is less than 25");
+    }
+}
