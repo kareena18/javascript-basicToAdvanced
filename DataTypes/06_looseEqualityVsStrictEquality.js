8fbd65bfe02d32b94b12462ec4d1b3492e29a916
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-23 17:57:34 +0000

    loose equality vs strict equality

---
+++ DataTypes/06_looseEqualityVsStrictEquality.js
@@ -0,0 +1,10 @@
+//loose equality(==) or strict equality(===)
+
+//loose equality
+let age =22//number
+console.log(age=="22")//focus only on value but not type
+console.log(age!="22");
+
+//strict equality operator
+console.log(age==="22")//focus on both value and its type
+console.log(age!=="22");
