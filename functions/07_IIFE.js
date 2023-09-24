d9d181e81046dafc11f80eee46cff374e4bb66a1
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-24 10:55:15 +0530

    IIFE

---
+++ functions/07_IIFE.js
@@ -0,0 +1,9 @@
+//function 
+
+//immediately invoked function expression
+//executed only once
+
+//now this is expression
+(function(name){
+  console.log("this function will never executed again",name);
+})("IIFE");//library initialize
