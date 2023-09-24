7e1aea3a0817d844ea42f67a4b768849c263933c
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-24 23:33:36 +0530

    Math Object

---
+++ objects/06_MathObject.js
@@ -0,0 +1,10 @@
+//math object built in object in javascript
+console.log(Math);
+
+let number =7.83422222222222;
+console.log(Math.round(number));//round to nearest integer
+console.log(Math.floor(number));//round to down
+console.log(Math.ceil(number));//round to up
+console.log(Math.trunc(number));//remove decimal part
+
+console.log(Math.round(Math.random()*100));
