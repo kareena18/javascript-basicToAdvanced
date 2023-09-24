ceb8bf0f831c17e85e79cd79f820196e38f2a6e4
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-24 10:12:05 +0530

    function and function expression

---
+++ functions/01_functionAndFunctionExpression.js
@@ -0,0 +1,21 @@
+//functions
+//it is simply a piece of code which we can use in our program many times
+
+//it is just like a variable holding some piece of code
+
+//declaration
+function nameOfFunction(){
+  console.log("function has been declared");
+}
+//function call
+nameOfFunction();
+nameOfFunction();
+
+//function expression
+let fun = function(){//also called anonymous function
+  console.log("example of function expression");
+}
+//calling function expression
+fun();
+fun();
+fun();
