1455af057f1c384be012be696ef5c07e3cd8d00d
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-25 20:28:55 +0530

    DOM Introduction

---
+++ DOM/01_DOMintroduction.js
@@ -0,0 +1,7 @@
+//javascript DOM(document object model)
+//created by browser as the html load into the browser
+//browser create object of html called "document" object
+//in document object there is model of complete html in tree like structure
+
+console.log(document);
+console.log(document.URL);
