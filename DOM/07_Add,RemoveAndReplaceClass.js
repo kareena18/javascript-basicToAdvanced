6121a46c30d1698bdf027464d2ee2fc8cd2f0374
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-25 21:27:57 +0530

    Add, Remove And Replace Class

---
+++ DOM/07_Add,RemoveAndReplaceClass.js
@@ -0,0 +1,11 @@
+//add,remove and replace class of element
+let he = document.querySelector('h1');
+
+//add the class
+he.classList.add("newClass");
+
+//remove the class
+he.classList.remove("newClass");
+
+//replace the class
+he.classList.replace("main","newClass");
