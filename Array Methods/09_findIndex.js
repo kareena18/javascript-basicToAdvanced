0f0d377469e42cc1059cb142d1c007e20cc49a4a
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-26 10:54:00 +0530

    findIndex

---
+++ Array Methods/09_findIndex.js
@@ -0,0 +1,10 @@
+//findindex method
+//it return index of that array element who first pass the test otherwise -1
+
+let age =[11,45,32,87,30,67,41,63]
+
+let result=age.findIndex(students=>{
+  return students>46;
+})
+console.log(result);
+//can't find return -1
