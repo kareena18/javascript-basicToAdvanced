5c26ed825e679589d63517d4f75e5357c4eac518
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-26 12:38:46 +0530

    Local Storage intro

---
+++ LocalStorage/01_ LocalStorageintro.js
@@ -0,0 +1,10 @@
+//local storage
+
+//in previous todo list task when an refreash the browser we lose our current state data
+//store and retrive data from database
+//for now our focus is in local storage
+//store and retrive data from local storage
+//is an api provided by browser to store data inside browser
+
+console.log(window.localStorage);
+console.log(localStorage);
