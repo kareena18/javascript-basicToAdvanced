d23aa5bcb0ffa84d65cf709acc1a467b3faf5130
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-26 11:58:42 +0530

    Date And Time Methods

---
+++ Date/02_DateAndTimeMethods.js
@@ -0,0 +1,22 @@
+//date and time methods
+
+let completeDateAndTime = new Date();
+console.log(completeDateAndTime);
+
+//get full year
+console.log(completeDateAndTime.getFullYear());
+
+//get month
+console.log(completeDateAndTime.getMonth());//0 to 11
+
+//get week day
+console.log(completeDateAndTime.getDay())//0 to 6
+
+//date
+console.log(completeDateAndTime.getDate());
+
+//hours, minutes, seconds
+console.log(completeDateAndTime.getHours());
+console.log(completeDateAndTime.getMinutes());
+console.log(completeDateAndTime.getSeconds());
+console.log(completeDateAndTime.getMilliseconds());
