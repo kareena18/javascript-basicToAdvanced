c21acddcf043be16a251998207bdc3636f56df99
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-26 10:15:06 +0530

    slice

---
+++ Array Methods/02_slice.js
@@ -0,0 +1,11 @@
+//array methods 
+//slice method 
+//used to extract the part of the array
+//array.slice(startindex,lastindex(excluded))
+//return new array of extracted elements
+
+let num = [3,4,6,8,9,23,57];
+let extractarray = num.slice(1,4);//1-3
+//num.slice(1)//1 till end
+console.log(num);
+console.log(extractarray);
