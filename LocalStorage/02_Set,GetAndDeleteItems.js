f5d172d073993ef769603968f16ce45f24ad7160
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-26 12:47:49 +0530

    Set, Get and delete items

---
+++ LocalStorage/02_Set,GetAndDeleteItems.js
@@ -0,0 +1,20 @@
+//local storage
+
+//get items
+//set items
+//update item
+
+//set the item in local storage
+localStorage.setItem('passion','programming');
+localStorage.setItem('age','21');
+console.log(localStorage);
+
+//get item in local storage
+
+console.log(localStorage.getItem('age'));
+
+//update
+localStorage.setItem('passion',"coding");//overwrite the previous
+
+//remove the item
+localStorage.removeItem('passion');
