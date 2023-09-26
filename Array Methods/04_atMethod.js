381a66d21b69ef77bd8d502bd1a96edfa7412a87
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-26 10:27:42 +0530

    at

---
+++ Array Methods/04_atMethod.js
@@ -0,0 +1,14 @@
+//at methods
+
+let nums=[23,5,6,74,53,11,89,67];
+console.log(nums[0]);
+console.log(nums.at(1));
+
+//getting last element of the array
+console.log(nums[nums.length-1]);
+console.log(nums.slice(-1)[0]);
+console.log(nums.at(-1));
+
+//at method also work with strings
+let name="karmy";
+console.log(name.at(-2));
